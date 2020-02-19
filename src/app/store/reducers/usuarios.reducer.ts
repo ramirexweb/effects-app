c0d014d0cf8f46f052c78fa2febe856872185454
ramirex <ramirex@gmail.com>
2020-02-19 13:17:37 -0400

    reducer de usuarios

---
+++ src/app/store/reducers/usuarios.reducer.ts
@@ -0,0 +1,42 @@
+import { Usuario } from 'src/app/models/usuario.model';
+import * as fromUsuarios from '../actions';
+
+export interface UsuariosState {
+  users: Usuario[];
+  loaded: boolean;
+  loading: boolean;
+  Rerror: any;
+}
+
+const estadoInicial: UsuariosState = {
+  users: [],
+  loaded: false,
+  loading: false,
+  Rerror: null
+};
+
+
+export function usuariosReducer( state = estadoInicial, action: fromUsuarios.ususariosActions): UsuariosState {
+  switch ( action.type ) {
+    case fromUsuarios.CARGAR_USUARIOS:
+      return {
+        ...state,
+        loading: true
+      };
+    case fromUsuarios.CARGAR_USUARIOS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        users: [...action.usuarios]
+      };
+    case fromUsuarios.CARGAR_USUARIOS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        Rerror: action.payload
+      };
+    default: return state;
+  }
+}
